70b57638fcfa749b36732c2160738cf15dde3d6a
renee-coder <71918527+renee-coder@users.noreply.github.com>
2020-12-18 17:35:02 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+noseX=0;
+noseY=0;
+rightWristX=0;
+difference=0;
+leftWristX=0;
 function setup(){
     video=createCapture(VIDEO);
     video.size(550,550);
@@ -11,6 +16,11 @@ function setup(){
 }
 function draw(){
     background('#4a32a8');
+
+    document.getElementById("square_side").innerHTML = "Width and Height of the square will be = "+difference+"px";
+    fill('#fcffcf');
+    stroke('#fff799')
+    square(noseX, noseY, difference);
 }
 function modelLoaded(){
     console.log('PoseNet Is Initialized!');
@@ -18,5 +28,15 @@ function modelLoaded(){
 function gotPoses(results){
     if(results.length > 0){
         console.log(results);
+        noseX=results[0].pose.nose.x;
+        noseY=results[0].pose.nose.y;
+        console.log("noseX = "+noseX+" noseY = "+noseY);
+
+        rightWristX = results[0].pose.rightWrist.x;
+        leftWristX = results[0].pose.leftWrist.x;
+        difference= floor(leftWristX - rightWristX);
+
+        console.log("rightWristX = "+rightWristX+" leftWristX = "+leftWristX+" difference = "+difference);
+
     }
 }
